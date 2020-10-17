88752896c2812ceb9159cd3c6b1955f9cbd0c0ea
koo bohyeon <goosee115@gmail.com>
2020-10-18 00:32:01 +0900

    feature: calculate 함수 테스트 코드 작성

--- src/util/calculate.test.js
+++ src/util/calculate.test.js
@@ -1,5 +1,9 @@
 import { calculate, toPostfix } from './calculate'
 
+test('(1+2)*(1+2) 값은 9', () => {
+  calculate('(1+2)*(1+2)').toBe(9)
+})
+
 test('후위 표기법 테스트', () => {
   expect(
     toPostfix(['(', '1', '+', '2', ')', '*', '(', '1', '+', '2', ')'])
