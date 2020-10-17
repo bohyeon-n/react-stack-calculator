dd395845a08913b85dcb3031874aad06ef41694c
koo bohyeon <goosee115@gmail.com>
2020-10-18 00:33:26 +0900

    feature: 후위 표기법 계산 테스트 코드 작성

--- src/util/calculate.test.js
+++ src/util/calculate.test.js
@@ -1,4 +1,4 @@
-import { calculate, toPostfix } from './calculate'
+import { calculate, toPostfix, calcPostfix } from './calculate'
 
 test('(1+2)*(1+2) 값은 9', () => {
   calculate('(1+2)*(1+2)').toBe(9)
@@ -8,4 +8,8 @@ test('후위 표기법 테스트', () => {
   expect(
     toPostfix(['(', '1', '+', '2', ')', '*', '(', '1', '+', '2', ')'])
   ).toEqual(['1', '2', '+', '1', '2', '+', '*'])
+})
+
+test('후위 표기법 계산 테스트', () => {
+  expect(calcPostfix(['1', '2', '+', '1', '2', '+', '*']).toBe(9))
 })
