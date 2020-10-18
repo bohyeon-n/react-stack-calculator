344cf2c5d3595b089474f9faf381609a7778db32
koo bohyeon <goosee115@gmail.com>
2020-10-18 20:09:52 +0900

    feature: click 시 usernmae 가져오기 테스트
    비동기 함수 테스트 코드 연습을 위한 테스트용 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Calculator from './containers/Calculator'
 
 function App() {
+  const [username, setUsername] = useState('')
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://e6e2c81c-c22b-48cf-9e42-fd4cfd887b17.mock.pstmn.io/username`
+    )
+
+    const data = await response.json()
+
+    setUsername(data.username)
+  }
+
+  const onClickBtn = () => {
+    fetchData()
+  }
+
   return (
     <div className="App">
       <h1>계산기</h1>
+      <button onClick={e => onClickBtn()}>loading user</button>
+      <div>{username}</div>
       <Calculator />
     </div>
   )
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { cleanup, render } from '@testing-library/react'
+import {
+  cleanup,
+  render,
+  fireEvent,
+  waitForElement
+} from '@testing-library/react'
 
 import App from './App'
 
@@ -8,4 +13,18 @@ afterEach(cleanup)
 it('App.js: 계산기가 노출된다.', () => {
   const { queryByText } = render(<App />)
   expect(queryByText('계산기')).toBeInTheDocument()
+})
+
+it('App.js: button을 click하면 사용자 이름이 노출된다.', async () => {
+  //given
+  const result = {
+    username: 'stranger'
+  }
+
+  //when
+  const { getByText } = render(<App />)
+  fireEvent.click(getByText('loading user'))
+
+  //then
+  await waitForElement(() => getByText(result.username))
 })
