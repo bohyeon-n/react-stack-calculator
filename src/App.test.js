644720f7a62f02e16074d2cbf0c9a88871f376cc
koo bohyeon <goosee115@gmail.com>
2020-10-18 20:38:25 +0900

    feature: 테스트용으로 작성한 필요없는 기능 삭제

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  cleanup,
-  render,
-  fireEvent,
-  waitForElement
-} from '@testing-library/react'
+import { cleanup, render } from '@testing-library/react'
 
 import App from './App'
 
@@ -13,18 +8,4 @@ afterEach(cleanup)
 it('App.js: 계산기가 노출된다.', () => {
   const { queryByText } = render(<App />)
   expect(queryByText('계산기')).toBeInTheDocument()
-})
-
-it('App.js: button을 click하면 사용자 이름이 노출된다.', async () => {
-  //given
-  const result = {
-    username: 'stranger'
-  }
-
-  //when
-  const { getByText } = render(<App />)
-  fireEvent.click(getByText('loading user'))
-
-  //then
-  await waitForElement(() => getByText(result.username))
 })
